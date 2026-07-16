4a934f042040242141d3b0140477d821fffefa25
Arpan Mukherjee <119566371+Arpanmukherjee3110@users.noreply.github.com>
2026-07-16 23:58:31 +0530

    Refactor rotary encoder handling and display updates

--- pot_duty_manipulation.ino
+++ pot_duty_manipulation.ino
@@ -7,93 +7,94 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-// Rotary Encoder Pins
-#define CLK 25
-#define DT 26
-#define SW 27
+#define SDA_PIN 21
+#define SCL_PIN 22
 
-int lastCLK;
-float setVoltage = 7.0;
+#define CLK 18
+#define DT 19
+#define SW 5
 
-const float minVoltage = 7.0;
-const float maxVoltage = 24.0;
-const float step = 0.1;
+HardwareSerial DSPSerial(2);    
+int encoderValue = 7;
+int lastCLKState;
 
 void setup()
 {
     Serial.begin(115200);
 
+    DSPSerial.begin(115200, SERIAL_8N1, 16, 17);
+
     pinMode(CLK, INPUT_PULLUP);
     pinMode(DT, INPUT_PULLUP);
     pinMode(SW, INPUT_PULLUP);
 
-    lastCLK = digitalRead(CLK);
+    Wire.begin(SDA_PIN, SCL_PIN);
 
-    Wire.begin(21,22);
-
-    if(!display.begin(SSD1306_SWITCHCAPVCC,0x3C))
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
     {
-        while(1);
+        while (1);
     }
 
     display.clearDisplay();
-    display.display();
+
+    lastCLKState = digitalRead(CLK);
+
+    showValue();
+
+    sendVoltage();
 }
 
 void loop()
 {
     int currentCLK = digitalRead(CLK);
 
-    if(currentCLK != lastCLK && currentCLK == LOW)
+    if (currentCLK != lastCLKState)
     {
-        if(digitalRead(DT) != currentCLK)
+        if (currentCLK == LOW)
         {
-            setVoltage += step;
+            if (digitalRead(DT) != currentCLK)
+            {
+                if (encoderValue < 24)
+                    encoderValue++;
+            }
+            else
+            {
+                if (encoderValue > 7)
+                    encoderValue--;
+            }
+
+            showValue();
+
+            sendVoltage();
         }
-        else
-        {
-            setVoltage -= step;
-        }
-
-        if(setVoltage > maxVoltage)
-            setVoltage = maxVoltage;
-
-        if(setVoltage < minVoltage)
-            setVoltage = minVoltage;
-
-        updateDisplay();
     }
 
-    lastCLK = currentCLK;
-
-    if(digitalRead(SW) == LOW)
-    {
-        Serial.print("Selected Voltage = ");
-        Serial.println(setVoltage);
-
-        delay(300);
-    }
+    lastCLKState = currentCLK;
 }
 
-void updateDisplay()
+void showValue()
 {
     display.clearDisplay();
 
+    display.setTextColor(SSD1306_WHITE);
+
     display.setTextSize(1);
-    display.setTextColor(WHITE);
-
-    display.setCursor(0,0);
-    display.println("Buck Converter");
-
-    display.drawLine(0,12,128,12,WHITE);
-
-    display.setCursor(0,22);
-    display.print("Set Voltage");
+    display.setCursor(10, 10);
+    display.println("Output Voltage");
 
     display.setTextSize(2);
-    display.setCursor(15,40);
-    display.print(setVoltage,1);
-    display.print("V");
+    display.setCursor(35, 30);
+    display.print(encoderValue);
+    display.print(" V");
 
     display.display();
+}
+
+void sendVoltage()
+{
+    DSPSerial.print(encoderValue);
+    DSPSerial.print('\n');
+
+    Serial.print("Sent to DSP: ");
+    Serial.println(encoderValue);
 }
